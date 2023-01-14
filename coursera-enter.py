a21da9cbbea81a8076c170178e1968b24993e9a6
Alejandro Dinas <99763884+orleans47@users.noreply.github.com>
2023-01-14 10:22:30 -0500

    Add files via upload

---
+++ coursera-enter.py
@@ -0,0 +1,12 @@
+#! python3
+# coursera-enter.py 
+
+# Importing the required modules
+import webbrowser as wbs
+import sys
+
+# Get the input from the user in the command line
+    # Open the website
+wbs.open("https://www.coursera.org/learn/analytics-tableau/home/week/2")
+sys.exit("\n Good luck with your studies My Lord!")
+
