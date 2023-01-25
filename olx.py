4221b1e613325e00b42024d8c069b39c4cc92e4d
Alejandro Dinas <99763884+orleans47@users.noreply.github.com>
2023-01-25 16:13:41 -0500

    Add files via upload

--- olx.py
+++ olx.py
@@ -1,3 +1,7 @@
+import requests
+from time import sleep
+from PIL import Image
+import io
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as ec
@@ -26,12 +30,33 @@ for i in range(10):
     except:
         break
 
+# Scroll the pages to load the images
+driver.execute_script("window.scrollTo({top: 0, behavior: 'smooth'})")
+sleep(5)
+driver.execute_script("window.scrollTo({top: 20000, behavior: 'smooth'})")
+sleep(5)
+
+
 # Lista de todos los celulares
 celulares = driver.find_elements(By.XPATH, "//li[@data-aut-id= 'itemBox']")
 
-for celular in celulares:
+for i, celular in enumerate(celulares):
     precio = celular.find_element(By.XPATH, ".//span[@data-aut-id = 'itemPrice']").text
     print(precio)
     descripcion = celular.find_element(By.XPATH,".//span[@data-aut-id = 'itemTitle']").text
     print(descripcion)
+
+    try:
+        url = celular.find_element(By.XPATH, ".//img").get_attribute("src")
+        image_content = requests.get(url).content
+
+        image_file = io.BytesIO(image_content)
+        image = Image.open(image_file).convert("RGB")
+        path = "C:/Users/Alejandro Dinas.DESKTOP-B98QDHD/Documents/python/images/" + str(i) +".jpg"
+        with open(path, "wb") as f:
+            image.save(f,"JPEG", quality= 85)
+
+    except:
+        print("error")
+
 
